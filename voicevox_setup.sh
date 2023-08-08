42ebd99736adf112c1e903e9b81df5ae56e012e9
Gai Nakatogawa <nakatogawagai@gmail.com>
2023-08-08 21:58:35 +0900

    Add installer

---
+++ voicevox_setup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+curl -sSfL https://raw.githubusercontent.com/VOICEVOX/voicevox_core/8cf307df4412dc0db0b03c6957b83b032770c31a/scripts/downloads/download.sh | bash -s
+cd voicevox_core
+wget https://github.com/VOICEVOX/voicevox_core/releases/download/0.14.1/voicevox_core-0.14.1+cpu-cp38-abi3-linux_x86_64.whl
+pip install voicevox_core-0.14.1+cpu-cp38-abi3-linux_x86_64.whl
+wget https://raw.githubusercontent.com/VOICEVOX/voicevox_core/406f6c41408836840b9a38489d0f670fb960f412/example/python/run.py
