cecc5c70418ded17da4a7a577962f7c3065228b6
Gai Nakatogawa <nakatogawagai@gmail.com>
2023-08-08 23:40:58 +0900

    Add jtalk checker

--- voicevox_ros2/run.py
+++ voicevox_ros2/run.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+## VOICEVOX LIBS
 import dataclasses
 import json
 import logging
@@ -8,42 +11,28 @@ from typing import Tuple
 import voicevox_core
 from voicevox_core import AccelerationMode, AudioQuery, VoicevoxCore
 
+# Other pkgs
+import os
 
 def main() -> None:
-    logging.basicConfig(format="[%(levelname)s] %(name)s: %(message)s")
-    logger = logging.getLogger(__name__)
-    logger.setLevel("DEBUG")
-    logging.getLogger("voicevox_core_python_api").setLevel("DEBUG")
-    logging.getLogger("voicevox_core").setLevel("DEBUG")
-
     (acceleration_mode, open_jtalk_dict_dir, text, out, speaker_id) = parse_args()
-
-    logger.debug("%s", f"{voicevox_core.METAS=}")
-    logger.debug("%s", f"{voicevox_core.SUPPORTED_DEVICES=}")
-
-    logger.info("%s", f"Initializing ({acceleration_mode=}, {open_jtalk_dict_dir=})")
     core = VoicevoxCore(
         acceleration_mode=acceleration_mode, open_jtalk_dict_dir=open_jtalk_dict_dir
     )
-
-    logger.debug("%s", f"{core.is_gpu_mode=}")
-
-    logger.info("%s", f"Loading model {speaker_id}")
     core.load_model(speaker_id)
-
-    logger.debug("%s", f"{core.is_model_loaded(0)=}")
-
-    logger.info("%s", f"Creating an AudioQuery from {text!r}")
     audio_query = core.audio_query(text, speaker_id)
-
-    logger.info("%s", f"Synthesizing with {display_as_json(audio_query)}")
     wav = core.synthesis(audio_query, speaker_id)
-
     out.write_bytes(wav)
-    logger.info("%s", f"Wrote `{out}`")
+    print("%s", f"Wrote `{out}`")
 
 
 def parse_args() -> Tuple[AccelerationMode, Path, str, Path, int]:
+    #home_path = os.getenv('HOME')
+    jtalk_path = os.getenv('JTALK_PATH')
+
+    if jtalk_path is None:
+        print("ERROR! env JTALK_PATH is not exist. please define it")
+        os._exit(-1)
     argparser = ArgumentParser()
     argparser.add_argument(
         "--mode",
@@ -53,13 +42,13 @@ def parse_args() -> Tuple[AccelerationMode, Path, str, Path, int]:
     )
     argparser.add_argument(
         "--dict-dir",
-        default="./open_jtalk_dic_utf_8-1.11",
+        default= jtalk_path,
         type=Path,
         help="Open JTalkの辞書ディレクトリ",
     )
     argparser.add_argument(
         "--text",
-        default="この音声は、ボイスボックスを使用して、出力されています。",
+        default="この音声は、ボイスボックスロスツーを使用して、出力されています。",
         help="読み上げさせたい文章",
     )
     argparser.add_argument(
