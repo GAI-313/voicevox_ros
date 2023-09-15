e6354c3d3f34a5a3d65436a373394fec623aee22
Gai Nakatogawa <nakatogawagai@gmail.com>
2023-09-15 10:29:39 +0900

    英単語発音に対応

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,7 @@
 import rospy
 import functools
 
+import re
 import sys
 import argparse
 import os
@@ -24,9 +25,50 @@ from voicevox_ros.srv import Speaker_srv, Speaker_srvResponse
 
 speaker = core = None
 
+# 英語辞書作成
+dict_path = os.getenv('KANAENG_PATH')
+dict = {}
+with open(dict_path, mode='r', encoding='utf-8') as f:
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+        if i >= 6:
+            line_list = line.replace('\n', '').split(' ')
+            dict[line_list[0]] = line_list[1]
+# 短縮形
+reduction=[["It\'s","イッツ"],["I\'m","アイム"],["You\'re","ユーァ"],["He\'s","ヒーィズ"],["She\'s","シーィズ"],["We\'re","ウィーアー"],["They\'re","ゼァー"],["That\'s","ザッツ"],["Who\'s","フーズ"],["Where\'s","フェアーズ"],["I\'d","アイドゥ"],["You\'d","ユードゥ"],["I\'ve","アイブ"],["I\'ll","アイル"],["You\'ll","ユール"],["He\'ll","ヒール"],["She\'ll","シール"],["We\'ll","ウィール"]]
+
+def eng_to_kana(text):
+    # 読み上げ可能単語を変換
+    text = text.replace("+"," プラス ").replace("＋"," プラス ").replace("-"," マイナス ").replace("="," イコール ").replace("＝"," イコール ")
+    # No.2、No6みたいに、No.の後に数字が続く場合はノーではなくナンバーと読む
+    text = re.sub(r'No\.([0-9])',"ナンバー\\1",text)
+    text = re.sub(r'No([0-9])',"ナンバー\\1",text)
+    # 短縮形の処理
+    for red in reduction: text = text.replace(red[0]," "+red[1]+" ")
+    # this is a pen.のように、aの後に半角スペース、続いてアルファベットの場合、エーではなくアッと呼ぶ
+    text = re.sub(r'a ([a-zA-Z])',"アッ \\1",text)
+    # 文を区切る文字は消してはダメなので、前後に半角スペースを挟む
+    text = text.replace("."," . ").replace("。"," 。 ").replace("!"," ! ").replace("！"," ！ ")
+    # アルファベットとアルファベット以外が近接している時、その間に半角スペースを挟む（この後、英単語を単語ごとに区切るための前準備）
+    text_l=list(text)
+    for i in range(len(text))[::-1][:-1]:
+        if re.compile("[a-zA-Z]").search(text_l[i]) and re.compile("[^a-zA-Z]").search(text_l[i-1]): text_l.insert(i," ")
+        elif re.compile("[^a-zA-Z]").search(text_l[i]) and re.compile("[a-zA-Z]").search(text_l[i+-1]): text_l.insert(i," ")
+    # 半角スペースや読まなくて良い文字で区切り、各単語の英語をカタカナに変換
+    text_split = re.split('[ \,\*\-\_\=\(\)\[\]\'\"\&\$　]',text)
+    for i in range(len(text_split)):
+        if str.upper(text_split[i]) in dict:
+            text_split[i] = dict[str.upper(text_split[i])]
+
+    return (" ".join(text_split))
+
+
 def voice_generator(id, text, path=None):
     global out
 
+    # 英単語からカタカナに変換
+    text = eng_to_kana(text)
+    text = text.replace(" ", "")
 
     env_jtalk = os.getenv("JTALK_PATH")
     generate_path = __file__.replace("main.py", "voice/result.wav")
---
+++ voicevox_ros_pkg_setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+current_dir=$(dirname "$(readlink -f "$0")")
+
+cd $current_dir
+
+pypath=$current_dir/scripts/voicevox_ros_common
+
+# bashrc
+search_string="#voicevox_ros setting
+export LD_LIBRARY_PATH=\\\"\$LD_LIBRARY_PATH:\$current_dir/voicevox_core
+export JTALK_PATH=\"\$current_dir/voicevox_core/open_jtalk_dic_utf_8-1.11"
+export KANAENG_PATH=\"\$current_dir/voicevox_core/bep-eng.dic\"
+
+bashrc_path=~/.bashrc
+
+if grep -qF "$search_string" "$bashrc_path"; then
+    :
+else
+    pip install playsound
+    echo "LISTEN! : THIS PACKAGE NEED SOMEN PACKAGES. PLEASE ENTER THE PASSWORD"
+    sudo apt update; sudo apt install -y python3-gst-1.0
+    cd $current_dir/voicevox_core
+    pip install voicevox_core-*.whl
+    echo "#voicevox_ros setting" >> ~/.bashrc
+    echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:$current_dir/voicevox_core\"" >> ~/.bashrc
+    echo "export JTALK_PATH=\"$current_dir/voicevox_core/open_jtalk_dic_utf_8-1.11\"" >> ~/.bashrc
+    echo "export KANAENG_PATH=\"$current_dir/voicevox_core/bep-eng.dic\"" >> ~/.bashrc
+    echo "export PYTHONPATH=\"\$PYTHONPATH:$pypath\"" >> ~/.bashrc
+fi
+
+# voicevox pkg install
+if [ -d "$current_dir/voicevox_core" ]; then
+    echo "Voicevox_core is installed"    
+else
+    cd $current_dir
+    curl -sSfL https://raw.githubusercontent.com/VOICEVOX/voicevox_core/8cf307df4412dc0db0b03c6957b83b032770c31a/scripts/downloads/download.sh | bash -s
+    cd $current_dir/voicevox_core
+    wget https://github.com/VOICEVOX/voicevox_core/releases/download/0.14.1/voicevox_core-0.14.1+cpu-cp38-abi3-linux_x86_64.whl
+    wget https://fastapi.metacpan.org/source/MASH/Lingua-JA-Yomi-0.01/lib/Lingua/JA/bep-eng.dic
+fi
